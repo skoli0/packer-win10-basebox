adb8913fd99e452c3d25f4d8283d26c97fb997bb
Sandeep Koli <30632063+skoli0@users.noreply.github.com>
2021-06-08 09:09:33 +0530

    Update build script and packerfile

--- build_windows_10.sh
+++ build_windows_10.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 packer build --only=$1-iso \
-    --var disk_size=262144 \
-    --var iso_url=./iso/Windows.iso \
-    --var iso_checksum=sha256:DDF91DF5BDF2230446EB77A959B7555C59D79F6BC7C622F7E16D31A9BA34A5BF \
+    --var disk_size=20480 \
+    --var iso_url=./iso/Win10_20H2_English_x64.iso \
+    --var iso_checksum=sha256:e793f3c94d075b1aa710ec8d462cee77fde82caf400d143d68036f72c12d9a7e \
     windows_10.json
